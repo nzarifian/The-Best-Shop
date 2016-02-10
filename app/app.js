9ce62fcec771d58886e3fd788e5b91428444425c
haehanna <hanna92@gmail.com>
2016-02-10 10:43:59 -0500

    app.js skeleton

--- app/app.js
+++ app/app.js
@@ -1,1 +1,33 @@
+var app = angular.module('shopApp', ['ngRoute']);
 
+app.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'site/partials/login.html',
+      controller: 'LoginCtrl as ctrl'
+    })
+    .when('/profile', {
+      templateUrl: 'site/partials/profile.html',
+      controller: 'ProfileCtrl as ctrl',
+      resolve:{
+          });
+        }
+      }
+    })
+    .otherwise({
+      redirectTo: '/login'
+    });
+
+
+  $httpProvider.interceptors.push(function() {
+    return {
+      'request': function(config) {
+        config.headers = config.headers || {};
+        if (localStorage.authToken) {
+          config.headers.Authorization = localStorage.authToken;
+        }
+        return config;
+      }
+    };
+  });
+});

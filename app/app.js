2004494474655ed0d66494a51ecf102a3d0fa4db
nzarifian <naz.zarifian@gmail.com>
2016-02-10 11:11:06 -0500

    adding url paths and ctrl as ctrls on app.js

--- app/app.js
+++ app/app.js
@@ -1,21 +1,56 @@
-var app = angular.module('shopApp', ['ngRoute']);
+'use strict';
 
-app.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/login', {
-      templateUrl: 'site/partials/login.html',
-      controller: 'LoginCtrl as ctrl'
+var app = angular.module('shopApp',[
+    'ui.router',
+  ]);
+
+app.config(function($stateProvider, $httpProvider,$urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('/admin', {
+      templateUrl: 'site/partials/admin.html',
+      controller: 'AuthCtrl as ctrl'
     })
-    .when('/profile', {
-      templateUrl: 'site/partials/profile.html',
-      controller: 'ProfileCtrl as ctrl',
-      resolve:{
-          });
-        }
-      }
+    .state('/dashboard', {
+      templateUrl: 'site/partials/admin-main.html',
+      controller: 'AdminCtrl as ctrl'
+    })
+    .state('/addedit', {
+      templateUrl: 'site/partials/admin-addedit.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+    .state('/orders', {
+      templateUrl: 'site/partials/admin-orders.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+    .state('/main', {
+      templateUrl: 'site/partials/shop-main.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+    .state('/product', {
+      templateUrl: 'site/partials/shop-product.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+    .state('/search', {
+      templateUrl: 'site/partials/shop-search.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+    .state ('/cart', {
+      templateUrl: 'site/partials/shop-cart.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+    .state('/checkout', {
+      templateUrl: 'site/partials/shop-checkout.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+    .state('/submitOrder' {
+      templateUrl:'site/partials/shop-submit.html',
+      controller: 'OrderCtrl as ctrl'
     })
     .otherwise({
-      redirectTo: '/login'
+      redirectTo: '/main'
     });
 
 

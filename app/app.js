83c577f2913e565798da77c7e7b1138b1a41ef74
melissalsy <melissaleesy@gmail.com>
2016-02-10 15:28:54 -0500

    fixing

--- app/app.js
+++ app/app.js
@@ -1,21 +1,106 @@
-var app = angular.module('shopApp', ['ngRoute']);
+'use strict';
 
-app.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/login', {
-      templateUrl: 'site/partials/login.html',
-      controller: 'LoginCtrl as ctrl'
+var app = angular.module('shopApp',[
+    'ui.router',
+  ]);
+
+app.config(function($stateProvider, $httpProvider,$urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  // .state('shop',{
+  //   url:'/',
+  //   templateUrl:'site/partials/shop-main.html',
+  //   controller:'ShopCtrl as ctrl',
+  //   //TODO #3 resolve products before main page load
+  //   resolve: {
+  //     products: function(productSrv){
+  //       return productSrv.getProducts();
+  //     }
+  //   }
+  // })
+
+    .state('admin.login', {
+      url:'/admin',
+      templateUrl: 'site/partials/admin-login.html',
+      controller: 'AuthCtrl as ctrl'
     })
-    .when('/profile', {
-      templateUrl: 'site/partials/profile.html',
-      controller: 'ProfileCtrl as ctrl',
-      resolve:{
-          });
-        }
-      }
+
+    .state('admin.dashboard', {
+      url:'/dashboard',
+      templateUrl: 'site/partials/admin-main.html',
+      controller: 'AdminCtrl as ctrl'
     })
+
+    .state('admin.inventory', {
+      url:'/addedit',
+      templateUrl: 'site/partials/admin-addedit.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('inventory.add', {
+      url:'/add',
+      templateUrl: 'site/partials/admin-add.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('/edit',{
+      url:'inventory.edit',
+      templateUrl: 'site/partials/admin-edit.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('admin.orders', {
+      url:'/orders',
+      templateUrl: 'site/partials/admin-orders.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('orders.updateOrder',{
+      url:'/updateOrder',
+      templateUrl: 'site/partials/admin-ordersUpdate.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('shop', {
+      url:'/main',
+      templateUrl: 'site/partials/shop-main.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+
+    .state('shop.product', {
+      url:'/product',
+      templateUrl: 'site/partials/shop-product.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+
+    .state('product.search', {
+      url:'/search',
+      templateUrl: 'site/partials/shop-search.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+
+    .state ('cart', {
+      url:'/cart',
+      templateUrl: 'site/partials/shop-cart.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('checkout', {
+      url:'/checkout',
+      templateUrl: 'site/partials/shop-checkout.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
+    .state('submitOrder' {
+      url:'/submitOrder',
+      templateUrl:'site/partials/shop-submit.html',
+      controller: 'OrderCtrl as ctrl'
+    })
+
     .otherwise({
-      redirectTo: '/login'
+      redirectTo: '/main'
     });
 
 

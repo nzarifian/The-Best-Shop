e5ebdd30cb7537e9f320dbdabb182c5b72f68185
haehanna <hanna92@gmail.com>
2016-02-10 16:02:00 -0500

    omg

--- app/app.js
+++ app/app.js
@@ -1,14 +1,25 @@
 'use strict';
 
-var app = angular.module('shopApp',[
-    'ui.router',
-  ]);
+var app = angular.module('shopApp',['ui.router']);
 
 app.config(function($stateProvider, $httpProvider,$urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/main');
 
   $stateProvider
+
+      .state('shop', {
+      url:'/main',
+      templateUrl: 'site/partials/shop-main.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+
+    .state('shop.product', {
+      url:'/product',
+      templateUrl: 'site/partials/shop-product.html',
+      controller: 'ProductCtrl as ctrl'
+    })
+
   // .state('shop',{
   //   url:'/',
   //   templateUrl:'site/partials/shop-main.html',
@@ -21,88 +32,75 @@ app.config(function($stateProvider, $httpProvider,$urlRouterProvider) {
   //   }
   // })
 
-    .state('admin.login', {
-      url:'/admin',
-      templateUrl: 'site/partials/admin-login.html',
-      controller: 'AuthCtrl as ctrl'
-    })
+    // .state('admin.login', {
+    //   url:'/admin',
+    //   templateUrl: 'site/partials/admin-login.html',
+    //   controller: 'AuthCtrl as ctrl'
+    // })
 
-    .state('admin.dashboard', {
-      url:'/dashboard',
-      templateUrl: 'site/partials/admin-main.html',
-      controller: 'AdminCtrl as ctrl'
-    })
+    // .state('admin.dashboard', {
+    //   url:'/dashboard',
+    //   templateUrl: 'site/partials/admin-main.html',
+    //   controller: 'AdminCtrl as ctrl'
+    // })
 
-    .state('admin.inventory', {
-      url:'/addedit',
-      templateUrl: 'site/partials/admin-addedit.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('admin.inventory', {
+    //   url:'/addedit',
+    //   templateUrl: 'site/partials/admin-addedit.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('inventory.add', {
-      url:'/add',
-      templateUrl: 'site/partials/admin-add.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('inventory.add', {
+    //   url:'/add',
+    //   templateUrl: 'site/partials/admin-add.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('/edit',{
-      url:'inventory.edit',
-      templateUrl: 'site/partials/admin-edit.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('/edit',{
+    //   url:'inventory.edit',
+    //   templateUrl: 'site/partials/admin-edit.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('admin.orders', {
-      url:'/orders',
-      templateUrl: 'site/partials/admin-orders.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('admin.orders', {
+    //   url:'/orders',
+    //   templateUrl: 'site/partials/admin-orders.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('orders.updateOrder',{
-      url:'/updateOrder',
-      templateUrl: 'site/partials/admin-ordersUpdate.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('orders.updateOrder',{
+    //   url:'/updateOrder',
+    //   templateUrl: 'site/partials/admin-ordersUpdate.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('shop', {
-      url:'/main',
-      templateUrl: 'site/partials/shop-main.html',
-      controller: 'ProductCtrl as ctrl'
-    })
+    // .state('product.search', {
+    //   url:'/search',
+    //   templateUrl: 'site/partials/shop-search.html',
+    //   controller: 'ProductCtrl as ctrl'
+    // })
 
-    .state('shop.product', {
-      url:'/product',
-      templateUrl: 'site/partials/shop-product.html',
-      controller: 'ProductCtrl as ctrl'
-    })
+    // .state ('cart', {
+    //   url:'/cart',
+    //   templateUrl: 'site/partials/shop-cart.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('product.search', {
-      url:'/search',
-      templateUrl: 'site/partials/shop-search.html',
-      controller: 'ProductCtrl as ctrl'
-    })
+    // .state('checkout', {
+    //   url:'/checkout',
+    //   templateUrl: 'site/partials/shop-checkout.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state ('cart', {
-      url:'/cart',
-      templateUrl: 'site/partials/shop-cart.html',
-      controller: 'OrderCtrl as ctrl'
-    })
+    // .state('submitOrder' {
+    //   url:'/submitOrder',
+    //   templateUrl:'site/partials/shop-submit.html',
+    //   controller: 'OrderCtrl as ctrl'
+    // })
 
-    .state('checkout', {
-      url:'/checkout',
-      templateUrl: 'site/partials/shop-checkout.html',
-      controller: 'OrderCtrl as ctrl'
-    })
-
-    .state('submitOrder' {
-      url:'/submitOrder',
-      templateUrl:'site/partials/shop-submit.html',
-      controller: 'OrderCtrl as ctrl'
-    })
-
-    .otherwise({
-      redirectTo: '/main'
-    });
-
+   // .otherwise({
+   //   redirectTo: '/main'
+   //  });
 
   $httpProvider.interceptors.push(function() {
     return {

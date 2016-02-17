028f3b6ad189ff252a8f1ba29667d99fc6b755ac
melissalsy <melissaleesy@gmail.com>
2016-02-17 09:33:15 -0500

    wednesday

--- app/site/ctrl/order.ctrl.js
+++ app/site/ctrl/order.ctrl.js
@@ -52,8 +52,9 @@ OrderCtrl.prototype.goToCart = function(){
     ctrl.$state.go('shop.cart');
 }
 
-OrderCtrl.prototype.reviewOrder = function(){
+OrderCtrl.prototype.reviewOrder = function(cartProduct){
     var ctrl = this; 
+    ctrl.cartProduct = cartProduct;
     ctrl.cartSrv.cart.push(cartProduct);
 
     var customer = {
